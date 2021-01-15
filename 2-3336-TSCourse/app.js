75b7a4455f67d437172bee6a563b6112ba1c41f7
nellyXinwei <nelsonalbajr@yahoo.com>
2021-01-16 00:29:37 +0800

    1:19:14 - at array types

--- 2-3336-TSCourse/app.js
+++ 2-3336-TSCourse/app.js
@@ -1,5 +1,12 @@
 var person = {
     name: 'Name',
-    age: 10
+    age: 10,
+    hobbies: ['hobby1', 'hobby2']
 };
+var favoriteActivities;
+favoriteActivities = ['activity1', 1];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby);
+}
--- 2-3336-TSCourse/app.ts
+++ 2-3336-TSCourse/app.ts
@@ -1,10 +1,19 @@
-const person:  {
-  name:string;
-  age:number; 
+const person:{
+  name:string,
+  age:number,
+  hobbies:string[]
 }={
   name: 'Name',
-  age: 10
+  age: 10,
+  hobbies: ['hobby1','hobby2']
 
 }
 
+let favoriteActivities:any[];
+favoriteActivities = ['activity1',1]
+
 console.log(person.name);
+
+for(const hobby of person.hobbies){
+  console.log(hobby);
+}
